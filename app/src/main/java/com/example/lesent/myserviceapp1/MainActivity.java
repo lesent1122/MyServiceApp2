d7c0a5d5a1b732ebd112f00575f38f3dc0676ead
LESENT <lesent>
2016-01-07 14:21:46 +0800

    raw人

--- app/src/main/java/com/example/lesent/myserviceapp1/MainActivity.java
+++ app/src/main/java/com/example/lesent/myserviceapp1/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+
+
+
+
+
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -14,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
     }
 
 
